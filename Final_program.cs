c05315f286df00d38311704ce53697305077f9b7
Anastasia <denisovaanastasia202@gmail.com>
2023-11-06 00:48:52 +0300

    Ввели цикл, в котором программа проверяет каждый  элемент  массива на наличие соблюдения заданного условия

--- Final_program.cs
+++ Final_program.cs
@@ -4,3 +4,17 @@
 //массивами.
 
 ﻿string[] array1 = new string[8] {"count", "yes", "king", "12", "control", "0..", "//", "no"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+if(array1[i].Length <= 3)
+{
+array2[count] = array1[i];
+count++;
+}
+}
+}
