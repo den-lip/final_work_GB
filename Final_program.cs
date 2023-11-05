e738e3943e220fcb7d3d2ab92be1f55cf7b983ff
Anastasia <denisovaanastasia202@gmail.com>
2023-11-06 00:52:37 +0300

    Программа переносит в новый массив все элементы старого, которые соответствуют нашему условию(<=3)

--- Final_program.cs
+++ Final_program.cs
@@ -18,3 +18,15 @@ count++;
 }
 }
 }
+void PrintArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write("{array[i]} ");
+}
+Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
+//end
+//
